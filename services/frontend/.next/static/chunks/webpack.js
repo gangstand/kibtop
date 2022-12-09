98165472246c993143f293ce29617056f12e6fb0
gangfv <ganggstand@gmail.com>
2022-12-09 22:20:06 +0300

    del next

--- services/frontend/.next/static/chunks/webpack.js
+++ services/frontend/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ef4f438d497104a6"; }
+/******/ 		__webpack_require__.h = function() { return "3928f3f6cd66474b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
