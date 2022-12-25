b58418cba61f5f5a6261a7ab6a111efe0c7753bb
gangfv <ganggstand@gmail.com>
2022-12-25 19:36:00 +0300

    local host

--- services/frontend/services/Instance.js
+++ services/frontend/services/Instance.js
@@ -4,7 +4,7 @@ export const BASE_URL = 'http://127.0.0.1:8000/api/v1'
 
 
 export const instance = axios.create({
-    baseURL: 'http://94.250.251.138:8000/api/v1/',
+    baseURL: 'http://127.0.0.1:8000/api/v1',
     headers: {
         
     },
