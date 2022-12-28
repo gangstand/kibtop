886cd4916e58bdd82901b74f2ed3c32b35f8afef
gangfv <ganggstand@gmail.com>
2022-12-28 21:33:51 +0300

    fix docker

--- services/frontend/services/Instance.js
+++ services/frontend/services/Instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = 'http://94.250.251.138:8000/api/v1/'
+export const BASE_URL = 'http://127.0.0.1:8000/api/v1/'
 
 
 export const instance = axios.create({
