1f4c87430d49c5b4ec49e233baeb9c0b460bda4e
gangfv <ganggstand@gmail.com>
2023-01-05 17:57:29 +0300

    full auto lang post models

--- services/backend/sections/views/fashion/views_fashion.py
+++ services/backend/sections/views/fashion/views_fashion.py
@@ -3,6 +3,9 @@ from drf_multiple_model.pagination import MultipleModelLimitOffsetPagination
 from drf_multiple_model.views import ObjectMultipleModelAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from translatepy import Translator
+
 from sections.models import (
     FashionFull, FashionFullViewsUser, FashionFullFavouritesUser
 )
@@ -67,6 +70,59 @@ class FashionFullAPIListCreate(generics.CreateAPIView):
     queryset = model_fashion
     serializer_class = FashionFullSerializerDetail
     permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def post(self, request, *args, **kwargs):
+        request_data = request.data
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(data=request_data, context={'request': request})
+
+        request_data_set = []
+        for lang in ['_ru', '_en', '_tr']:
+            request_data_set.append([s for s in filter(lambda x: lang in x, [i for i in request_data])])
+        request_data_set = [x for x in request_data_set if x != []][0]
+
+        request_data = [request_data[i] for i in request_data_set]
+
+        lang = request_data_set[0][-2:]
+        if lang == "tr":
+            lang_ru = [Translator().translate(i, 'ru', 'tr').result for i in request_data]
+            lang_en = [Translator().translate(i, 'en', 'tr').result for i in request_data]
+            if serializer.is_valid():
+                serializer.save(
+                    title_en=lang_en[0],
+                    title_ru=lang_ru[0],
+                    category_en=lang_en[1],
+                    category_ru=lang_ru[1],
+                    sub_category_en=lang_en[2],
+                    sub_category_ru=lang_ru[2],
+                )
+        elif lang == "en":
+            lang_ru = [Translator().translate(i, 'ru', 'en').result for i in request_data]
+            lang_tr = [Translator().translate(i, 'tr', 'en').result for i in request_data]
+            if serializer.is_valid():
+                serializer.save(
+                    title_tr=lang_tr[0],
+                    title_ru=lang_ru[0],
+                    category_tr=lang_tr[1],
+                    category_ru=lang_ru[1],
+                    sub_category_tr=lang_tr[2],
+                    sub_category_ru=lang_ru[2],
+                )
+        elif lang == "ru":
+            lang_en = [Translator().translate(i, 'en', 'ru').result for i in request_data]
+            lang_tr = [Translator().translate(i, 'tr', 'ru').result for i in request_data]
+            if serializer.is_valid():
+                serializer.save(
+                    title_tr=lang_tr[0],
+                    title_en=lang_en[0],
+                    category_tr=lang_tr[1],
+                    category_en=lang_en[1],
+                    sub_category_tr=lang_tr[2],
+                    sub_category_en=lang_en[2],
+                )
+        else:
+            return Response({'message': 'bad'})
+        return Response({'message': 'good'})
 
 
 class FashionFullAPIUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
--- services/backend/sections/views/realty/views_realty.py
+++ services/backend/sections/views/realty/views_realty.py
@@ -3,6 +3,9 @@ from drf_multiple_model.pagination import MultipleModelLimitOffsetPagination
 from drf_multiple_model.views import ObjectMultipleModelAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from translatepy import Translator
+
 from sections.models import (
     RealtyFull, RealtyFullFavouritesUser, RealtyFullViewsUser
 )
@@ -15,7 +18,7 @@ from sections.service import (
 )
 from sections.utils import query_list_lang
 
-model_realty = RealtyFull.objects.filter(publisher=True,)
+model_realty = RealtyFull.objects.filter(publisher=True, )
 
 
 class RealtyLimitPagination(MultipleModelLimitOffsetPagination):
@@ -67,6 +70,71 @@ class RealtyFullAPIListCreate(generics.CreateAPIView):
     queryset = model_realty
     serializer_class = RealtyFullSerializerDetail
     permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def post(self, request, *args, **kwargs):
+        request_data = request.data
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(data=request_data, context={'request': request})
+
+        request_data_set = []
+        for lang in ['_ru', '_en', '_tr']:
+            request_data_set.append([s for s in filter(lambda x: lang in x, [i for i in request_data])])
+        request_data_set = [x for x in request_data_set if x != []][0]
+
+        request_data = [request_data[i] for i in request_data_set]
+
+        lang = request_data_set[0][-2:]
+        if lang == "tr":
+            lang_ru = [Translator().translate(i, 'ru', 'tr').result for i in request_data]
+            lang_en = [Translator().translate(i, 'en', 'tr').result for i in request_data]
+            if serializer.is_valid():
+                serializer.save(
+                    title_en=lang_en[0],
+                    title_ru=lang_ru[0],
+                    category_en=lang_en[1],
+                    category_ru=lang_ru[1],
+                    sub_category_en=lang_en[2],
+                    sub_category_ru=lang_ru[2],
+                    all_old_new_en=lang_en[3],
+                    all_old_new_ru=lang_ru[3],
+                    number_rooms_en=lang_en[4],
+                    number_rooms_ru=lang_ru[4],
+                )
+        elif lang == "en":
+            lang_ru = [Translator().translate(i, 'ru', 'en').result for i in request_data]
+            lang_tr = [Translator().translate(i, 'tr', 'en').result for i in request_data]
+            if serializer.is_valid():
+                serializer.save(
+                    title_tr=lang_tr[0],
+                    title_ru=lang_ru[0],
+                    category_tr=lang_tr[1],
+                    category_ru=lang_ru[1],
+                    sub_category_tr=lang_tr[2],
+                    sub_category_ru=lang_ru[2],
+                    all_old_new_tr=lang_tr[3],
+                    all_old_new_ru=lang_ru[3],
+                    number_rooms_tr=lang_tr[4],
+                    number_rooms_ru=lang_ru[4],
+                )
+        elif lang == "ru":
+            lang_en = [Translator().translate(i, 'en', 'ru').result for i in request_data]
+            lang_tr = [Translator().translate(i, 'tr', 'ru').result for i in request_data]
+            if serializer.is_valid():
+                serializer.save(
+                    title_en=lang_en[0],
+                    title_tr=lang_tr[0],
+                    category_en=lang_en[1],
+                    category_tr=lang_tr[1],
+                    sub_category_en=lang_en[2],
+                    sub_category_tr=lang_tr[2],
+                    all_old_new_en=lang_en[3],
+                    all_old_new_tr=lang_tr[3],
+                    number_rooms_en=lang_en[4],
+                    number_rooms_tr=lang_tr[4],
+                )
+        else:
+            return Response({'message': 'bad'})
+        return Response({'message': 'good'})
 
 
 class RealtyFullAPIUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
