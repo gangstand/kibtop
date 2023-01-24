c2751b4d60c456d9fd81c0dfbf1001c7f2724959
gang <ganggstand@gmail.com>
2023-01-24 13:15:20 +0300

    fix deals

--- services/backend/accounts/serializer.py
+++ services/backend/accounts/serializer.py
@@ -4,19 +4,19 @@ from djoser.serializers import UserSerializer, UserCreateSerializer, UserCreateP
 class UserRegistrationSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         fields = (
-            'username', 'email', 'phone', 'password', 'first_name', 'addres', 'upload_user'
+            'username', 'email', 'phone', 'password', 'first_name', 'addres', 'upload_user', 'deals'
         )
 
 
 class UserAPISerializer(UserSerializer):
     class Meta(UserSerializer.Meta):
         fields = (
-            'id', 'username', 'phone', 'email', 'first_name', 'addres', 'upload_user'
+            'id', 'username', 'phone', 'email', 'first_name', 'addres', 'upload_user', 'deals'
         )
 
 
 class UserAPICreatePasswordRetypeSerializer(UserCreatePasswordRetypeSerializer):
     class Meta(UserCreatePasswordRetypeSerializer.Meta):
         fields = (
-            'username', 'email', 'phone', 'first_name', 'addres', 'upload_user', 'password'
+            'username', 'email', 'phone', 'first_name', 'addres', 'upload_user', 'deals', 'password'
         )
