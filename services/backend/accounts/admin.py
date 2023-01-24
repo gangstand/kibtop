8a6efce0f87e2ee267d363734222895836af2613
gang <ganggstand@gmail.com>
2023-01-24 13:09:11 +0300

    fix auth

--- services/backend/accounts/admin.py
+++ services/backend/accounts/admin.py
@@ -7,11 +7,12 @@ from accounts.models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['email', 'auth_provider', 'first_name', 'is_superuser', 'is_active', ]
+    list_display = ['username', 'email', 'auth_provider', 'first_name', 'is_superuser', 'is_active', ]
     fieldsets = (
         (None, {
-            'fields': ('username', 'email', 'phone', 'password', 'first_name', 'addres', 'upload_user', 'auth_provider',
-                       'is_superuser', 'is_active',)
+            'fields': (
+            'username', 'email', 'phone', 'password', 'first_name', 'addres', 'upload_user',
+            'auth_provider', 'is_superuser', 'is_active',)
         }),
     )
 
