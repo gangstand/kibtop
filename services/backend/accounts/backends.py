db66e54901114567a66d6f59c5d0de2cab83a240
gang <ganggstand@gmail.com>
2023-01-24 19:35:46 +0300

    fix

--- services/backend/accounts/backends.py
+++ services/backend/accounts/backends.py
@@ -14,11 +14,11 @@ class AuthBackend(object):
         except CustomUser.DoesNotExist:
             return None
 
-    def authenticate(self, request, email, password):
+    def authenticate(self, request, username, password):
 
         try:
             user = CustomUser.objects.get(
-                Q(email=email)
+                Q(username=username) | Q(email=username) | Q(phone=username)
             )
 
         except CustomUser.DoesNotExist:
